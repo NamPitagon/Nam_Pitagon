b24a719f5dc8f707329560ece4212bf92e003f87
namnv <namnv@pitagon.vn>
2022-07-22 17:37:02 +0700

    update code

---
+++ code/ConvertData/ConvertData/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+// khai báo 2 thư viện để làm việc với form
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
--- code/FontEnd/Angular/Article/src/app/core/models/user.model.ts
+++
@@ -1,7 +0,0 @@
-export interface User{
-    email: string;
-    username: string;
-    token: string;
-    bio: string;
-    image: string;
-}
