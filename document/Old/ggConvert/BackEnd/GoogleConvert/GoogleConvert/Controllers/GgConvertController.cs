3ef3133d49c7a2d6dd1ee522176e9e2286019be7
namnv <namnv@pitagon.vn>
2022-07-28 08:41:27 +0700

    upload code cũ

---
+++ document/Old/ggConvert/BackEnd/GoogleConvert/GoogleConvert/Controllers/GgConvertController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace GoogleConvert.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GgConvertController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public GgConvertController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+    }
+}
---
+++ document/Old/ggConvert/BackEnd/GoogleConvert/GoogleConvert/Dto/ResponseData.cs
@@ -0,0 +1,8 @@
+﻿namespace GoogleConvert.Dto
+{
+    public class ResponseData
+    {
+        public bool success { get; set; }
+        public string value { get; set; }
+    }
+}
